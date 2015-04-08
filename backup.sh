356791bb0ff2cae971ecee373fba10cd7e7083de
Christoph Rissner <cri@visotech.at>
2015-04-08 15:32:13 +0200

    automatic commit

--- backup.sh
+++ backup.sh
@@ -1,4 +1,57 @@
 #!/bin/bash
 set -e
-~/Desktop/flinkwork/backup.sh
-#~/work/flinkwork/docker/backup.sh
+#set -x
+
+LABEL=FLINKBACKUP
+MOUNTPATH=/media/${LABEL}
+export MOUNTPATH
+
+MOUNTED=0
+if [[ ! $(mount | grep ${LABEL}) ]]; then
+    echo "Mounting $MOUNTPATH"
+    mount ${MOUNTPATH}
+    MOUNTED=1
+fi
+
+function auto_add
+{
+    if [[ $1 = "auto_add" ]];
+    then
+        echo "automatic add in ${PWD}"
+        git add .
+    fi
+}
+
+function auto_commit
+{
+    if [[ $1 = "auto_commit" ]];
+    then
+        echo "automatic commit in ${PWD}"
+        git commit -am "automatic commit" || true
+    fi
+}
+
+function git_mirror
+{
+    REPO=$1
+    AUTOADD=$2
+    AUTOCOMMIT=$3
+    MIRROR=${MOUNTPATH}"/"$(basename ${REPO})".git"
+
+    cd ${REPO}
+    auto_add ${AUTOADD}
+    auto_commit ${AUTOCOMMIT}
+
+    echo "mirroring ${PWD} -> ${MIRROR}"
+    git push --mirror ${MIRROR}
+}
+
+git_mirror ~/opt/bin no_add auto_commit
+git_mirror ~/work/flinkwork/office auto_add auto_commit
+git_mirror ~/work/flinkwork/docker
+
+if [[ ${MOUNTED} -eq 1 ]]; then
+    echo "Unmounting $MOUNTPATH"
+    umount ${MOUNTPATH}
+fi
+
