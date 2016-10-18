8cced11be308f45f031eefdefbb7a80623e865e9
Christoph Rissner <cri@flinkwork.com>
2016-10-18 14:50:01 +0200

    gitlab: add pipeline status for MRs

--- gitlab.py
+++ gitlab.py
@@ -31,7 +31,10 @@ def get(path, reverse=False, **kw):
         r = s.get(base_url + path, params=params)
         if not r.ok:
             break
-        data.extend(r.json())
+        rdata = r.json()
+        if not isinstance(rdata, list):
+            return load_dt(rdata)
+        data.extend(rdata)
         page = r.headers.get("X-Next-Page", None)
         if not page:
             break
@@ -41,21 +44,41 @@ def get(path, reverse=False, **kw):
 
 projects = {_project["path_with_namespace"]:_project for _project in get("/projects/")}
 
+_pipelines = {}
+def project_pipelines(project_id):
+    if project_id not in _pipelines:
+        _pipelines[project_id] = get("/projects/{}/pipelines".format(project_id), reverse=True)
+    return _pipelines[project_id]
+
 now = datetime.datetime.now()
 for project in sorted(projects.values(), key=lambda p: p["last_activity_at"], reverse=True):
     mrs = get("/projects/{}/merge_requests".format(project["id"]), reverse=True, state="opened")
-
-    pipelines = get("/projects/{}/pipelines".format(project["id"]), reverse=True)[:3]
-    pipelines = [p for p in pipelines if (now - p["created_at"] < datetime.timedelta(days=5))]
+    pipelines = [p for p in project_pipelines(project["id"]) if (now - p["created_at"] < datetime.timedelta(days=5))][:3]
     if not (pipelines or mrs):
         continue
 
     print(project["path_with_namespace"], project["web_url"])
     for mr in mrs:
+        build_status = ""
+        pipeline_by_sha = {p["sha"]:p for p in project_pipelines(mr["source_project_id"])}
+        for commit in get("/projects/{}/merge_requests/{}/commits".format(project["id"], mr["id"])):
+            try:
+                build_status = pipeline_by_sha[commit["id"]]["status"]
+                break
+            except KeyError:
+                pass
         try:
             assignee = mr["assignee"]["name"]
         except (TypeError, KeyError):
             assignee = "nobody :-("
-        print("{:>10}".format(mr["id"]), "{:>10}".format(""), "{:>20}".format(human(mr["created_at"], 1)), "{:>10}".format(assignee), "  ", mr["title"])
+        print("{:>10}".format(mr["id"]),
+              "{:>10}".format(build_status),
+              "{:>20}".format(human(mr["created_at"], 1)),
+              "{:>20}".format(assignee),
+              mr["title"])
     for p in pipelines:
-        print("{:>10}".format(p["id"]), "{:>10}".format(p["status"]), "{:>20}".format(human(p["created_at"], 1)), "  ", p["ref"])
+        print("{:>10}".format(p["id"]),
+              "{:>10}".format(p["status"]),
+              "{:>20}".format(human(p["created_at"], 1)),
+              "{:>20}".format(""),
+              p["ref"])
